3595e0c0ff2f8b761229e8deff26e26ba0817c17
Faye Chen <xiaoxianfaye@hotmail.com>
2017-10-12 21:31:49 +0800

    TDD

---
+++ TDD/TDD Coding Practice String Transformer/Code/Java/src/fayelab/tdd/stringtransformer/reverse/BusinessLogicImpl.java
@@ -0,0 +1,70 @@
+package fayelab.tdd.stringtransformer.reverse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+
+public class BusinessLogicImpl implements BusinessLogic
+{
+    private static Map<String, Function<String, String>> transIdAndFuncMap = null;
+    
+    static
+    {
+        transIdAndFuncMap = new HashMap<>();
+        transIdAndFuncMap.put("Upper", str -> upper(str));
+        transIdAndFuncMap.put("Lower", str -> lower(str));
+        transIdAndFuncMap.put("TrimPrefixSpaces", str -> trimPrefixSpaces(str));
+        transIdAndFuncMap.put("Reverse", str -> reverse(str));
+    }
+    
+    @Override
+    public String transform(String sourceStr, List<String> transIds)
+    {
+        return transIds.stream().reduce(sourceStr, 
+                (resultStr, transId) -> transIdAndFuncMap.get(transId).apply(resultStr));
+    }
+
+    @Override
+    public List<String> getAllTransIds()
+    {
+        return transIdAndFuncMap.keySet().stream().collect(toList());
+    }
+
+    private static String lower(String str)
+    {
+        return str.toLowerCase();
+    }
+
+    private static String upper(String str)
+    {
+        return str.toUpperCase();
+    }
+
+    private static String trimPrefixSpaces(String str)
+    {
+        int firstNonSpaceCharIdx = findFirstNonSpaceCharIdx(str);
+        return firstNonSpaceCharIdx == -1 ? str : str.substring(firstNonSpaceCharIdx);
+    }
+
+    private static int findFirstNonSpaceCharIdx(String str)
+    {
+        int firstNonSpaceCharIdx = -1;
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) != ' ')
+            {
+                firstNonSpaceCharIdx = i;
+                break;
+            }
+        }
+        return firstNonSpaceCharIdx;
+    }
+    
+    private static String reverse(String str)
+    {
+        return new StringBuffer(str).reverse().toString();
+    }
+}
---
+++ TDD/TDD Coding Practice String Transformer/Code/Java/testsrc/fayelab/tdd/stringtransformer/reverse/ViewStub.java
@@ -0,0 +1,108 @@
+package fayelab.tdd.stringtransformer.reverse;
+
+import java.util.List;
+
+public class ViewStub implements View
+{
+    private List<String> availableTransIds;
+    private String selectedAvailableTransId;
+    private List<String> chainTransIds;
+    private String selectedChainTransId;
+    private String sourceStr;
+    private String resultStr;
+    private boolean isEmptySourceStrInputNotified;
+    private boolean isEmptyChainInputNotified;
+
+    @Override
+    public void presentAvailableTransIds(List<String> transIds)
+    {
+        availableTransIds = transIds;
+    }
+    
+    public List<String> getAvailableTransIds()
+    {
+        return availableTransIds;
+    }
+
+    @Override
+    public String getSelectedAvailableTransId()
+    {
+        return selectedAvailableTransId;
+    }
+
+    public void setSelectedAvailableTransId(String transId)
+    {
+        selectedAvailableTransId = transId;
+    }
+
+    @Override
+    public void presentChainTransIds(List<String> transIds)
+    {
+        chainTransIds = transIds;
+    }
+
+    public List<String> getChainTransIds()
+    {
+        return chainTransIds;
+    }
+
+    @Override
+    public String getSelectedChainTransId()
+    {
+        return selectedChainTransId;
+    }
+
+    public void setSelectedChainTransId(String transId)
+    {
+        selectedChainTransId = transId;
+    }
+
+    @Override
+    public String getSourceStr()
+    {
+        return sourceStr;
+    }
+    
+    public void setSourceStr(String str)
+    {
+        sourceStr = str;
+    }
+
+    @Override
+    public void presentResultStr(String str)
+    {
+        resultStr = str;
+    }
+
+    public String getResultStr()
+    {
+        return resultStr;
+    }
+
+    @Override
+    public void onEmptySourceStrInput()
+    {
+        isEmptySourceStrInputNotified = true;
+    }
+
+    public boolean isEmptySourceStrInputNotified()
+    {
+        return isEmptySourceStrInputNotified;
+    }
+
+    @Override
+    public void onEmptyChainInput()
+    {
+        isEmptyChainInputNotified = true;
+    }
+
+    public boolean isEmptyChainInputNotified()
+    {
+        return isEmptyChainInputNotified;
+    }
+
+    @Override
+    public void setPresenter(Presenter presenter)
+    {
+    }
+}
