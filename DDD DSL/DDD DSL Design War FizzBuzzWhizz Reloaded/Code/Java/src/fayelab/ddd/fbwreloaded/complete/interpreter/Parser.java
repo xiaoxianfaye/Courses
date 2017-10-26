817a9d50f0d2c3d5d4625df10fe3c95b0a8f1d70
xiaoxianfaye <xiaoxianfaye@hotmail.com>
2017-10-26 17:22:10 +0800

    DDD/DSL fbw reloaded

--- DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/src/fayelab/ddd/fbwreloaded/complete/interpreter/Parser.java
+++ DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/src/fayelab/ddd/fbwreloaded/complete/interpreter/Parser.java
@@ -1,12 +1,23 @@
 package fayelab.ddd.fbwreloaded.complete.interpreter;
 
-import static java.util.Arrays.asList;
-
 import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+import java.util.HashMap;
 
 public class Parser
 {
     private static final String DELIMETER = " ";
+    
+    static List<List<String>> tokenize(List<String> ruleDescs)
+    {        
+        return ruleDescs.stream().filter(line -> isNotBlank(line))
+                             .map(line -> asList(normalize(line).split(DELIMETER)))
+                             .collect(toList());
+    }
 
     static boolean isNotBlank(String line)
     {
@@ -18,13 +29,16 @@ public class Parser
         return line.trim().replaceAll(" +", DELIMETER);
     }
     
-    static List<String> tokenize(String normalizedLine)
-    {        
-        return asList(normalizedLine.split(DELIMETER));
+    static Rule parseTokens(List<List<String>> tokens)
+    {
+        Map<String, Rule> nameAndRules = new HashMap<>();
+        tokens.stream().forEach(ruleTokens -> parseRuleTokens(ruleTokens, nameAndRules));
+        return nameAndRules.get("spec");
     }
-    
-//    static void parseRuleTokens(List<String> ruleTokens, Map<String, Rule> nameMappingRule)
-//    {
-//        
-//    }
+
+    static void parseRuleTokens(List<String> ruleTokens, Map<String, Rule> nameAndRules)
+    {
+        // TODO Auto-generated method stub
+        return;
+    }
 }
--- DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/testsrc/fayelab/ddd/fbwreloaded/complete/interpreter/ParserTest.java
+++ DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/testsrc/fayelab/ddd/fbwreloaded/complete/interpreter/ParserTest.java
@@ -2,47 +2,81 @@ package fayelab.ddd.fbwreloaded.complete.interpreter;
 
 import junit.framework.TestCase;
 
+import java.util.List;
+
 import static fayelab.ddd.fbwreloaded.complete.interpreter.Parser.*;
 import static java.util.Arrays.asList;
 
 public class ParserTest extends TestCase
 {
-    public void test_isNotBlank()
-    {
-        assertFalse(isNotBlank(""));
-        assertFalse(isNotBlank(" "));
-        assertFalse(isNotBlank("  "));
-        assertTrue(isNotBlank("  123  "));
-    }
-    
-    public void test_normalize()
-    {
-        assertEquals("spec or r3 r2 r1 rd", normalize(" spec  or r3 r2 r1  rd  "));
-    }
+    private static final List<List<String>> SPEC_TOKENS = asList(
+            asList("r1_3", "atom", "times", "3", "to_fizz"),
+            asList("r1_5", "atom", "times", "5", "to_buzz"),
+            asList("r1_7", "atom", "times", "7", "to_whizz"),
+            asList("r1", "or", "r1_3", "r1_5", "r1_7"),
+            asList("r1_357", "and", "r1_3", "r1_5", "r1_7"),
+            asList("r1_35", "and", "r1_3", "r1_5"),
+            asList("r1_37", "and", "r1_3", "r1_7"),
+            asList("r1_57", "and", "r1_5", "r1_7"),
+            asList("r2", "or", "r1_357", "r1_35", "r1_37", "r1_57"),
+            asList("r3", "atom", "contains", "3", "to_fizz"),
+            asList("rd", "atom", "always_true", "to_str"),
+            asList("spec", "or", "r3", "r2", "r1", "rd"));
     
     public void test_tokenize()
     {
-        assertEquals(asList("spec", "or", "r3", "r2", "r1", "rd"), tokenize("spec or r3 r2 r1 rd"));
+        List<String> ruleDescs = asList(
+                "r1_3 atom times 3 to_fizz",
+                "r1_5 atom times 5 to_buzz",
+                "r1_7 atom times 7 to_whizz",
+                "",
+                "r1 or r1_3 r1_5 r1_7",
+                "",
+                "r1_357 and r1_3 r1_5 r1_7",
+                "r1_35 and r1_3 r1_5",
+                "r1_37 and r1_3 r1_7",
+                "r1_57 and r1_5 r1_7",
+                "",
+                "r2 or r1_357 r1_35 r1_37 r1_57",
+                "",
+                "r3 atom contains 3 to_fizz",
+                "",
+                "rd atom always_true to_str",
+                "  ",
+                " spec  or r3 r2 r1  rd  ");
+        
+        assertEquals(SPEC_TOKENS, tokenize(ruleDescs));
     }
     
-//    public void test_parse_atom()
-//    {
+    public void test_parseTokens()
+    {        
+//        List<List<String>> spec1 = asList(
+//                asList("r1_3", "atom", "times", "3", "to_fizz"),);
 //        
-//        Map<String, Rule> actual = new HashMap<>();
-//        parseRuleTokens(asList("r1_3", "atom", "times", "3", "to_fizz"), actual);
-//        parseRuleTokens(asList("r1_5", "atom", "times", "5", "to_buzz"), actual);
-//        parseRuleTokens(asList("r1_7", "atom", "times", "7", "to_whizz"), actual);
-//        parseRuleTokens(asList("r3", "atom", "contains", "3", "to_fizz"), actual);
-//        parseRuleTokens(asList("rd", "atom", "always_true", "to_str"), actual);
-//        
-//        Map<String, Rule> expected = new HashMap<>(); 
-//        expected.put("r1_3", atom(times(3), toFizz()));
-//        expected.put("r1_5", atom(times(5), toBuzz()));
-//        expected.put("r1_7", atom(times(7), toWhizz()));
-//        expected.put("r3", atom(contains(3), toFizz()));
-//        expected.put("rd", atom(alwaysTrue(), toStr()));
-//        assertMap(expected, actual);
-//    }
+//        assertEquals(SpecTool.spec(), parseTokens(SPEC_TOKENS));
+    }
+    
+//  public void test_parse_atom()
+//  {
+//      
+//      Map<String, Rule> actual = new HashMap<>();
+//      parseRuleTokens(asList("r1_3", "atom", "times", "3", "to_fizz"), actual);
+//      parseRuleTokens(asList("r1_5", "atom", "times", "5", "to_buzz"), actual);
+//      parseRuleTokens(asList("r1_7", "atom", "times", "7", "to_whizz"), actual);
+//      parseRuleTokens(asList("r3", "atom", "contains", "3", "to_fizz"), actual);
+//      parseRuleTokens(asList("rd", "atom", "always_true", "to_str"), actual);
+//      
+//      Map<String, Rule> expected = new HashMap<>(); 
+//      expected.put("r1_3", atom(times(3), toFizz()));
+//      expected.put("r1_5", atom(times(5), toBuzz()));
+//      expected.put("r1_7", atom(times(7), toWhizz()));
+//      expected.put("r3", atom(contains(3), toFizz()));
+//      expected.put("rd", atom(alwaysTrue(), toStr()));
+//      assertMap(expected, actual);
+//  }
+    
+    
+
     
 //    private void assertMap(Map<String, Rule> expected, Map<String, Rule> actual)
 //    {
