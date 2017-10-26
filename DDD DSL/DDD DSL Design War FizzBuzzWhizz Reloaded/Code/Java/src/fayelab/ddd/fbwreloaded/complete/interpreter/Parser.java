ec3b5bd162c3c6aa3311e5d5fc7cde6dbb94f640
xiaoxianfaye <xiaoxianfaye@hotmail.com>
2017-10-26 20:22:45 +0800

    DDD/DSL fbw reloaded

--- DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/src/fayelab/ddd/fbwreloaded/complete/interpreter/Parser.java
+++ DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/src/fayelab/ddd/fbwreloaded/complete/interpreter/Parser.java
@@ -2,15 +2,40 @@ package fayelab.ddd.fbwreloaded.complete.interpreter;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
 import java.util.HashMap;
 
+import static fayelab.ddd.fbwreloaded.complete.interpreter.SpecTool.*;
+
 public class Parser
 {
     private static final String DELIMETER = " ";
+    
+    private static Map<String, BiFunction<String, String, Predication>> predicationTypeAndFuncMap = null;
+    
+    static
+    {
+        predicationTypeAndFuncMap = new HashMap<>();
+        predicationTypeAndFuncMap.put("times", (type, param) -> times(Integer.parseInt(param)));
+        predicationTypeAndFuncMap.put("contains", (type, param) -> contains(Integer.parseInt(param)));
+        predicationTypeAndFuncMap.put("always_true", (type, param) -> alwaysTrue());
+    }
+    
+    private static Map<String, Supplier<Action>> actionTypeAndFuncMap = null;
+    
+    static
+    {
+        actionTypeAndFuncMap = new HashMap<>();
+        actionTypeAndFuncMap.put("to_fizz", () -> toFizz());
+        actionTypeAndFuncMap.put("to_buzz", () -> toBuzz());
+        actionTypeAndFuncMap.put("to_whizz", () -> toWhizz());
+        actionTypeAndFuncMap.put("to_str", () -> toStr());
+    }
     
     static List<List<String>> tokenize(List<String> ruleDescs)
     {        
@@ -28,17 +53,103 @@ public class Parser
     {
         return line.trim().replaceAll(" +", DELIMETER);
     }
-    
-    static Rule parseTokens(List<List<String>> tokens)
+
+    static Rule parseAtom(List<String> ruleTokens)
     {
-        Map<String, Rule> nameAndRules = new HashMap<>();
-        tokens.stream().forEach(ruleTokens -> parseRuleTokens(ruleTokens, nameAndRules));
-        return nameAndRules.get("spec");
+        String predicationType = extractPredicationType(ruleTokens);
+        String predicationParam = extractPredicationParam(ruleTokens, predicationType);
+        String actionType = extractActionType(ruleTokens, predicationType);
+
+        return atom(parsePredication(predicationType, predicationParam), parseAction(actionType));
+    }
+    
+    private static String extractPredicationType(List<String> ruleTokens)
+    {
+        return ruleTokens.get(2);
     }
 
-    static void parseRuleTokens(List<String> ruleTokens, Map<String, Rule> nameAndRules)
+    private static String extractPredicationParam(List<String> ruleTokens, String predicationType)
     {
-        // TODO Auto-generated method stub
-        return;
+        return "always_true".equals(predicationType) ? null : ruleTokens.get(3);
     }
+
+    private static String extractActionType(List<String> ruleTokens, String predicationType)
+    {
+        return "always_true".equals(predicationType) ? ruleTokens.get(3) : ruleTokens.get(4);
+    }
+    
+    private static Predication parsePredication(String predicationType, String predicationParam)
+    {
+        return predicationTypeAndFuncMap.get(predicationType).apply(predicationType, predicationParam);
+    }
+
+    private static Action parseAction(String actionType)
+    {
+        return actionTypeAndFuncMap.get(actionType).get();
+    }
+
+    static Rule parseOr(List<String> ruleTokens, Map<String, Rule> ruleNameAndRuleMap)
+    {
+        return or(parseRefRules(ruleTokens, ruleNameAndRuleMap));
+    }
+    
+    static Rule parseAnd(List<String> ruleTokens, Map<String, Rule> ruleNameAndRuleMap)
+    {
+        return and(parseRefRules(ruleTokens, ruleNameAndRuleMap));
+    }
+
+    private static List<Rule> parseRefRules(List<String> ruleTokens, Map<String, Rule> ruleNameAndRuleMap)
+    {
+        List<Rule> refRules = ruleTokens.subList(2, ruleTokens.size())
+                                        .stream()
+                                        .map(name -> ruleNameAndRuleMap.get(name)).collect(toList());
+        return refRules;
+    }
+    
+    static void parseRuleTokens(List<String> ruleTokens, Map<String, Rule> ruleNameAndRuleMap)
+    {
+        String ruleName = extractRuleName(ruleTokens);
+        String ruleType = extractRuleType(ruleTokens);
+        
+        Rule rule = null;
+        if("atom".equals(ruleType))
+        {
+            rule = parseAtom(ruleTokens);
+        }
+        else if("or".equals(ruleType))
+        {
+            rule = parseOr(ruleTokens, ruleNameAndRuleMap);
+        }
+        else
+        {
+            rule = parseAnd(ruleTokens, ruleNameAndRuleMap);
+        }
+        
+        ruleNameAndRuleMap.putIfAbsent(ruleName, rule);
+    }
+
+    private static String extractRuleName(List<String> ruleTokens)
+    {
+        return ruleTokens.get(0);
+    }
+
+    private static String extractRuleType(List<String> ruleTokens)
+    {
+        return ruleTokens.get(1);
+    }
+    
+//    static Rule parseTokens(List<List<String>> tokens)
+//    {
+//        Map<String, Rule> nameAndRules = new HashMap<>();
+//        tokens.stream().forEach(ruleTokens -> parseRuleTokens(ruleTokens, nameAndRules));
+//        return nameAndRules.get("spec");
+//    }
+    
+    
+
+//    static void parseRuleTokens(List<String> ruleTokens, Map<String, Rule> nameAndRules)
+//    {
+//        // TODO Auto-generated method stub
+//        return;
+//    }
 }
--- DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/testsrc/fayelab/ddd/fbwreloaded/complete/interpreter/ParserTest.java
+++ DDD DSL/DDD DSL Design War FizzBuzzWhizz Reloaded/Code/Java/testsrc/fayelab/ddd/fbwreloaded/complete/interpreter/ParserTest.java
@@ -3,9 +3,15 @@ package fayelab.ddd.fbwreloaded.complete.interpreter;
 import junit.framework.TestCase;
 
 import java.util.List;
+import java.util.Map;
 
 import static fayelab.ddd.fbwreloaded.complete.interpreter.Parser.*;
 import static java.util.Arrays.asList;
+
+import java.util.HashMap;
+
+import static fayelab.ddd.fbwreloaded.complete.interpreter.SpecTool.*;
+import static fayelab.ddd.fbwreloaded.complete.interpreter.RuleEqualityTool.equal;
 
 public class ParserTest extends TestCase
 {
@@ -22,6 +28,24 @@ public class ParserTest extends TestCase
             asList("r3", "atom", "contains", "3", "to_fizz"),
             asList("rd", "atom", "always_true", "to_str"),
             asList("spec", "or", "r3", "r2", "r1", "rd"));
+
+    private Rule r1_3;
+    private Rule r1_5;
+    private Rule r1_7;
+    private Map<String, Rule> ruleNameAndRuleMap;
+    
+    @Override
+    protected void setUp()
+    {
+        r1_3 = atom(times(3), toFizz());
+        r1_5 = atom(times(5), toBuzz());
+        r1_7 = atom(times(7), toWhizz());
+        
+        ruleNameAndRuleMap = new HashMap<String, Rule>();
+        ruleNameAndRuleMap.put("r1_3", r1_3);
+        ruleNameAndRuleMap.put("r1_5", r1_5);
+        ruleNameAndRuleMap.put("r1_7", r1_7);
+    }
     
     public void test_tokenize()
     {
@@ -48,6 +72,59 @@ public class ParserTest extends TestCase
         assertEquals(SPEC_TOKENS, tokenize(ruleDescs));
     }
     
+    public void test_parseAtom()
+    {
+        assertTrue(equal(atom(times(3), toFizz()), parseAtom(asList("r1_3", "atom", "times", "3", "to_fizz"))));
+        assertTrue(equal(atom(contains(3), toFizz()), parseAtom(asList("r3", "atom", "contains", "3", "to_fizz"))));
+        assertTrue(equal(atom(alwaysTrue(), toStr()), parseAtom(asList("rd", "atom", "always_true", "to_str"))));
+    }
+    
+    public void test_parseOr()
+    {
+        assertTrue(equal(or(r1_3, r1_5, r1_7), 
+                   parseOr(asList("or", "r1", "r1_3", "r1_5", "r1_7"), ruleNameAndRuleMap)));
+    }
+    
+    public void test_parseAnd()
+    {
+        assertTrue(equal(and(r1_3, r1_5, r1_7), 
+                   parseAnd(asList("and", "r1_357", "r1_3", "r1_5", "r1_7"), ruleNameAndRuleMap)));
+    }
+    
+    public void test_parseRuleTokens()
+    {
+        Map<String, Rule> actual = new HashMap<>();
+        
+        SPEC_TOKENS.stream().forEach(ruleTokens -> parseRuleTokens(ruleTokens, actual));
+
+        Map<String, Rule> expected = new HashMap<>(); 
+        expected.put("r1_3", r1_3);
+        expected.put("r1_5", r1_5);
+        expected.put("r1_7", r1_7);
+        
+        expected.put("r1", or(r1_3, r1_5, r1_7));
+        
+        Rule r1_357 = and(r1_3, r1_5, r1_7);
+        expected.put("r1_357", r1_357);
+        Rule r1_35 = and(r1_3, r1_5);
+        expected.put("r1_35", r1_35);
+        Rule r1_37 = and(r1_3, r1_7);
+        expected.put("r1_37", r1_37);
+        Rule r1_57 = and(r1_5, r1_7);
+        expected.put("r1_57", r1_57);
+        
+        expected.put("r2", or(r1_357, r1_35, r1_37, r1_57));
+        
+        Rule r3 = atom(contains(3), toFizz());
+        expected.put("r3", r3);
+        Rule rd = atom(alwaysTrue(), toStr());
+        expected.put("rd", rd);
+        
+        expected.put("spec", rd);
+        
+        assertMap(expected, actual);
+    }
+
     public void test_parseTokens()
     {        
 //        List<List<String>> spec1 = asList(
@@ -56,40 +133,10 @@ public class ParserTest extends TestCase
 //        assertEquals(SpecTool.spec(), parseTokens(SPEC_TOKENS));
     }
     
-//  public void test_parse_atom()
-//  {
-//      
-//      Map<String, Rule> actual = new HashMap<>();
-//      parseRuleTokens(asList("r1_3", "atom", "times", "3", "to_fizz"), actual);
-//      parseRuleTokens(asList("r1_5", "atom", "times", "5", "to_buzz"), actual);
-//      parseRuleTokens(asList("r1_7", "atom", "times", "7", "to_whizz"), actual);
-//      parseRuleTokens(asList("r3", "atom", "contains", "3", "to_fizz"), actual);
-//      parseRuleTokens(asList("rd", "atom", "always_true", "to_str"), actual);
-//      
-//      Map<String, Rule> expected = new HashMap<>(); 
-//      expected.put("r1_3", atom(times(3), toFizz()));
-//      expected.put("r1_5", atom(times(5), toBuzz()));
-//      expected.put("r1_7", atom(times(7), toWhizz()));
-//      expected.put("r3", atom(contains(3), toFizz()));
-//      expected.put("rd", atom(alwaysTrue(), toStr()));
-//      assertMap(expected, actual);
-//  }
-    
-    
-
-    
-//    private void assertMap(Map<String, Rule> expected, Map<String, Rule> actual)
-//    {
-//        Set<String> actualKeySet = actual.keySet();
-//        
-//        assertEquals(expected.keySet(), actualKeySet);
-//        
-//        expected.forEach((name, rule) -> {
-//            String expectedRuleStr = PrintInterpreter.interpret(rule);
-//            Rule actualRule = actual.get(name);
-//            assertNotNull(actualRule);
-//            String actualRuleStr = PrintInterpreter.interpret(actualRule);
-//            assertEquals(expectedRuleStr, actualRuleStr);
-//        });
-//    }
+    private void assertMap(Map<String, Rule> expected, Map<String, Rule> actual)
+    {
+        assertEquals(expected.keySet(), actual.keySet());
+        
+        expected.keySet().forEach(ruleName -> {assertTrue(equal(expected.get(ruleName), actual.get(ruleName)));});
+    }
 }
