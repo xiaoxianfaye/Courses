1382eefd5484ed5ec39bb98eb8386ba03485b98e
xiaoxianfaye <xiaoxianfaye@hotmail.com>
2017-12-07 20:16:50 +0800

    TDD Arithmetic Pythohn

--- TDD/TDD Coding Practice Arithmetic/Double Stack Algorithm/Code/Python/original/doublestackprocessor.py
+++ TDD/TDD Coding Practice Arithmetic/Double Stack Algorithm/Code/Python/original/doublestackprocessor.py
@@ -1,43 +1,67 @@
 import copy
 
 class DoubleStackProcessor(object):
+    operator_func_map = {'+':lambda x, y: x + y,
+                         '*':lambda x, y: x * y,
+                         '-':lambda x, y: x - y,
+                         '/':lambda x, y: x / y}
+
+    operator_priority_map = {'+':1, '-':1, '*':2, '/':2}
+    
     def __init__(self):
         self.operandstack = []
+        self.operatorstack = []
 
     def process(self, c):
-        self.operandstack.append(int(c))
+        if c.isdigit():
+            self.process_operand(c)
+        else:
+            self.process_operator(c)
+
+    def process_operand(self, c):
+        self.push_operand(int(c))
+
+    def process_operator(self, c):
+        while self.not_empty_operatorstack() and self.not_prior_to(c, self.top_operator()):
+            self.calc_once()
+
+        self.push_operator(c)
+
+    def not_empty_operatorstack(self):
+        return len(self.operatorstack) != 0
+
+    def not_prior_to(self, operator1, operator2):
+        return self.priority(operator1) <= self.priority(operator2)
+
+    def priority(self, operator):
+        return DoubleStackProcessor.operator_priority_map[operator]
+
+    def calc_once(self):
+        r_operand = self.pop_operand()
+        l_operand = self.pop_operand()
+        operator = self.pop_operator()
+        self.push_operand(self.apply(operator, l_operand, r_operand))
+
+    def apply(self, operator, l_operand, r_operand):
+        return DoubleStackProcessor.operator_func_map[operator](l_operand, r_operand)
+
+    def push_operand(self, operand):
+        self.operandstack.append(operand)
+
+    def pop_operand(self):
+        return self.operandstack.pop()
+
+    def push_operator(self, operator):
+        self.operatorstack.append(operator)
+
+    def pop_operator(self):
+        return self.operatorstack.pop()
+
+    def top_operator(self):
+        return self.operatorstack[-1]
 
     def _dump_operandstack(self):
         return copy.copy(self.operandstack)
 
-# class DoubleStackProcessor(object):
-#     def __init__(self):
-#         self.operandStack = []
-#         self.operatorStack = []
-
-#     def process(self, c):
-#         if c.isdigit():
-#             self.pushOperand(int(c))
-#         else:
-#             if self.not_empty_operatorstack():
-#                 rOperand = self.operandStack.pop()
-#                 lOperand = self.operandStack.pop()
-#                 operator = self.operatorStack.pop()
-#                 self.pushOperand(lOperand + rOperand)
-
-#             self.pushOperator(c)
-
-#     def not_empty_operatorstack(self):
-#         return len(self.operatorStack) != 0
-
-#     def pushOperand(self, operand):
-#         self.operandStack.append(operand)
-
-#     def pushOperator(self, operator):
-#         self.operatorStack.append(operator)
-
-#     def _dumpOperandStack(self):
-#         return copy.copy(self.operandStack)
-
-#     def _dumpOperatorStack(self):
-#         return copy.copy(self.operatorStack)
+    def _dump_operatorstack(self):
+        return copy.copy(self.operatorstack)
--- TDD/TDD Coding Practice Arithmetic/Double Stack Algorithm/Code/Python/original/tests/test_doublestackprocessor.py
+++ TDD/TDD Coding Practice Arithmetic/Double Stack Algorithm/Code/Python/original/tests/test_doublestackprocessor.py
@@ -3,42 +3,101 @@ import unittest
 from doublestackprocessor import DoubleStackProcessor
 
 class TestDoubleStackProcessor(unittest.TestCase):
+    def setUp(self):
+        self.dsp = DoubleStackProcessor()
+
     def test_num(self):
-        dsp = DoubleStackProcessor()
+        self.dsp.process('3')
 
-        dsp.process('3')
+        self.assertEquals([3], self.dsp._dump_operandstack())
 
-        self.assertEquals([3], dsp._dump_operandstack())
+    def test_num_plus(self):
+        self.dsp.process('3')
+        self.dsp.process('+')
 
-# import unittest
+        self.assertEquals([3], self.dsp._dump_operandstack())
+        self.assertEquals(['+'], self.dsp._dump_operatorstack())
 
-# from doublestackprocessor import DoubleStackProcessor
+    def test_num_plus_num_plus(self):
+        self.dsp.process('3')
+        self.dsp.process('+')
+        self.dsp.process('2')
+        self.dsp.process('+')        
 
-# class TestDoubleStackProcessor(unittest.TestCase):
-#     def setUp(self):
-#         self.dsp = DoubleStackProcessor()
+        self.assertEquals([5], self.dsp._dump_operandstack())
+        self.assertEquals(['+'], self.dsp._dump_operatorstack())
 
-#     def test_num(self):
-#         self.dsp.process('3')
+    def test_num_plus_num_multiply(self):
+        self.dsp.process('3')
+        self.dsp.process('+')
+        self.dsp.process('2')
+        self.dsp.process('*')        
 
-#         self.assertEquals([3], self.dsp._dumpOperandStack())
+        self.assertEquals([3, 2], self.dsp._dump_operandstack())
+        self.assertEquals(['+', '*'], self.dsp._dump_operatorstack())
 
-#     def test_num_plus(self):
-#         self.dsp.process('3')
-#         self.dsp.process('+')
+    def test_num_multiply_num_multiply(self):
+        self.dsp.process('3')
+        self.dsp.process('*')
+        self.dsp.process('2')
+        self.dsp.process('*')
+        
+        self.assertEquals([6], self.dsp._dump_operandstack())
+        self.assertEquals(['*'], self.dsp._dump_operatorstack())
 
-#         self.assertEquals([3], self.dsp._dumpOperandStack())
-#         self.assertEquals(['+'], self.dsp._dumpOperatorStack())
+    def test_num_multiply_num_plus(self):
+        self.dsp.process('3')
+        self.dsp.process('*')
+        self.dsp.process('2')
+        self.dsp.process('+')
+        
+        self.assertEquals([6], self.dsp._dump_operandstack())
+        self.assertEquals(['+'], self.dsp._dump_operatorstack())
 
-#     def test_num_plus_num_plus(self):
-#         self.dsp.process('3')
-#         self.dsp.process('+')
-#         self.dsp.process('2')
-#         self.dsp.process('+')        
+    def test_num_plus_num_multiply_num_plus(self):
+        self.dsp.process('3')
+        self.dsp.process('+')
+        self.dsp.process('2')
+        self.dsp.process('*')
+        self.dsp.process('3')
+        self.dsp.process('+')
+        
+        self.assertEquals([9], self.dsp._dump_operandstack())
+        self.assertEquals(['+'], self.dsp._dump_operatorstack())
 
-#         self.assertEquals([5], self.dsp._dumpOperandStack())
-#         self.assertEquals(['+'], self.dsp._dumpOperatorStack())
+    def test_num_plus_num_subtract(self):
+        self.dsp.process('3')
+        self.dsp.process('+')
+        self.dsp.process('2')
+        self.dsp.process('-')
+        
+        self.assertEquals([5], self.dsp._dump_operandstack())
+        self.assertEquals(['-'], self.dsp._dump_operatorstack())
+
+    def test_num_subtract_num_plus(self):
+        self.dsp.process('3')
+        self.dsp.process('-')
+        self.dsp.process('2')
+        self.dsp.process('+')
+        
+        self.assertEquals([1], self.dsp._dump_operandstack())
+        self.assertEquals(['+'], self.dsp._dump_operatorstack())
 
 
+    def test_num_subtract_num_divide(self):
+        self.dsp.process('4')
+        self.dsp.process('-')
+        self.dsp.process('2')
+        self.dsp.process('/')
+        
+        self.assertEquals([4, 2], self.dsp._dump_operandstack())
+        self.assertEquals(['-', '/'], self.dsp._dump_operatorstack())
 
-
+    def test_num_divide_num_plus(self):
+        self.dsp.process('4')
+        self.dsp.process('/')
+        self.dsp.process('2')
+        self.dsp.process('+')
+        
+        self.assertEquals([2], self.dsp._dump_operandstack())
+        self.assertEquals(['+'], self.dsp._dump_operatorstack())
