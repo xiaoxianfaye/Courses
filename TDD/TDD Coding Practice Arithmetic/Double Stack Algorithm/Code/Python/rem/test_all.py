a06058b5bd893c210100bcca1e82ca53971c431f
xiaoxianfaye <xiaoxianfaye@hotmail.com>
2017-12-08 11:09:55 +0800

    TDD Arithmetic Python

---
+++ TDD/TDD Coding Practice Arithmetic/Double Stack Algorithm/Code/Python/rem/test_all.py
@@ -0,0 +1,7 @@
+import unittest
+
+from tests.test_doublestackprocessor import TestDoubleStackProcessor
+from tests.test_expression import TestExpression
+
+if __name__ == '__main__':
+    unittest.main()
