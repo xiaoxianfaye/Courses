f87c93e66f0c2e386289a8a44629c181a3d32c7d
Faye Chen <xiaoxianfaye@hotmail.com>
2018-01-22 21:44:59 +0800

    DDD/DSL Layout Java Functional

---
+++ DDD DSL/DDD DSL Design War Layout/Code/Java/src/fayelab/ddd/layout/original/functional/Layout.java
@@ -0,0 +1,65 @@
+package fayelab.ddd.layout.original.functional;
+
+import java.awt.Container;
+import java.awt.Rectangle;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class Layout
+{
+    public static Rectangle rectangle(int left, int top, int width, int height)
+    {
+        return new Rectangle(left, top, width, height);
+    }
+    
+    public static Function<Rectangle, Consumer<Container>> button(String text)
+    {
+        JButton btn = new JButton(text);
+        return rectangle -> {
+            btn.setBounds(rectangle);
+            return container -> container.add(btn);
+        };
+    }
+    
+    public static Function<Rectangle, Consumer<Container>> textField()
+    {
+        JTextField textField = new JTextField();
+        return rectangle -> {
+            textField.setBounds(rectangle);
+            return container -> container.add(textField);
+        };
+    }
+    
+    public static Function<Rectangle, Consumer<Container>> beside(Function<Rectangle, Consumer<Container>> leftCmp, 
+            Function<Rectangle, Consumer<Container>> rightCmp, float ratio)
+    {
+        return outerRectangle -> container -> {
+            leftCmp.apply(rectangle((int)outerRectangle.getX(), 
+                                    (int)outerRectangle.getY(),
+                                    (int)(outerRectangle.getWidth() * ratio), 
+                                    (int)outerRectangle.getHeight())).accept(container);;
+            rightCmp.apply(rectangle((int)(outerRectangle.getX() + outerRectangle.getWidth() * ratio), 
+                                     (int)outerRectangle.getY(),
+                                     (int)(outerRectangle.getWidth() * (1 - ratio)), 
+                                     (int)outerRectangle.getHeight())).accept(container);
+        };
+    }
+    
+    public static Function<Rectangle, Consumer<Container>> above(Function<Rectangle, Consumer<Container>> upCmp, 
+            Function<Rectangle, Consumer<Container>> downCmp, float ratio)
+    {
+        return outerRectangle -> container -> {
+            upCmp.apply(rectangle((int)outerRectangle.getX(), 
+                                  (int)outerRectangle.getY(),
+                                  (int)outerRectangle.getWidth(), 
+                                  (int)(outerRectangle.getHeight() * ratio))).accept(container);
+            downCmp.apply(rectangle((int)outerRectangle.getX(), 
+                                    (int)(outerRectangle.getY() + outerRectangle.getHeight() * ratio),
+                                    (int)outerRectangle.getWidth(), 
+                                    (int)(outerRectangle.getHeight() * (1 - ratio)))).accept(container);
+        };
+    }
+}
---
+++ DDD DSL/DDD DSL Design War Layout/Code/Java/testsrc/fayelab/ddd/layout/globalparam/functional/LayoutTest.java
@@ -0,0 +1,71 @@
+package fayelab.ddd.layout.globalparam.functional;
+
+import java.awt.Container;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import junit.framework.TestCase;
+
+import static fayelab.ddd.layout.original.functional.Layout.*;
+
+public class LayoutTest extends TestCase
+{
+    private static final int TOLERANCE = 1;
+    
+    private ArrayList<Rectangle> rectangles;
+    private ArrayList<Container> containers;
+
+    protected void setUp()
+    {
+        this.rectangles = new ArrayList<>();
+        this.containers = new ArrayList<>();
+    }
+    
+    public void test_beside()
+    {
+        Function<Rectangle, Consumer<Container>> leftCmp = componentStub();
+        Function<Rectangle, Consumer<Container>> rightCmp = componentStub();
+        Container container = new Container();
+        
+        beside(leftCmp, rightCmp, 0.8f).apply(rectangle(20, 10, 300, 60)).accept(container);
+        
+        checkRectangle(rectangle(20, 10, 240, 60), rectangles.get(0));
+        checkRectangle(rectangle(260, 10, 60, 60), rectangles.get(1));
+        
+        assertSame(container, containers.get(0));
+        assertSame(container, containers.get(1));
+    }
+    
+    public void test_above()
+    {
+        Function<Rectangle, Consumer<Container>> upCmp = componentStub();
+        Function<Rectangle, Consumer<Container>> downCmp = componentStub();
+        Container container = new Container();
+        
+        above(upCmp, downCmp, 0.5f).apply(rectangle(20, 10, 300, 60)).accept(container);
+        
+        checkRectangle(rectangle(20, 10, 300, 30), rectangles.get(0));
+        checkRectangle(rectangle(20, 40, 300, 30), rectangles.get(1));
+        
+        assertSame(container, containers.get(0));
+        assertSame(container, containers.get(1));
+    }
+
+    private Function<Rectangle, Consumer<Container>> componentStub()
+    {
+        return rectangle -> {
+            rectangles.add(rectangle);
+            return container -> containers.add(container);
+        };
+    }
+    
+    private void checkRectangle(Rectangle expected, Rectangle actual)
+    {
+        assertEquals((int)expected.getX(), (int)actual.getX(), TOLERANCE);
+        assertEquals((int)expected.getY(), (int)actual.getY(), TOLERANCE);
+        assertEquals((int)expected.getWidth(), (int)actual.getWidth(), TOLERANCE);
+        assertEquals((int)expected.getHeight(), (int)actual.getHeight(), TOLERANCE);
+    }
+}
